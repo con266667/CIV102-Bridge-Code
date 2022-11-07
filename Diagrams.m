14977ef49fb22aff99901bdac59221c0d82d4f7f
Connor Wilson <cnwilson29@gmail.com>
2022-11-07 13:53:04 -0500

    multiple point loads

--- Diagrams.m
+++ Diagrams.m
@@ -9,7 +9,7 @@ x = linspace(0, L, n+1); % x-axis (0 to 1201)
 
 x_train = [52 228 392 568 732 908]; % Train Load Locations
 P_train=[1 1 1 1 1 1]*P/6; 
-n_train = 3;
+n_train = 1;
 
 SFDi = zeros(n_train, n+1); % 1 SFD for each train loc.
 BMDi = zeros(n_train, n+1); % 1 BMD for each train loc.
@@ -17,21 +17,27 @@ BMDi = zeros(n_train, n+1); % 1 BMD for each train loc.
 % Solve for SFD and BMD with the train at different locations
 
 for i = 1:n_train
-    location = fix(L / (n_train+1) * i); % start location of train (300, 600, 900)
+    %location = fix(L / (n_train+1) * i); % start location of train (300, 600, 900)
+    locations = x_train;
     
     % Reaction forces (Rearranged moment and Fy equations)
-    B_y = location * P / L;
-    A_y = P - B_y;
+    sum(locations .* P_train)
+    B_y = sum(locations .* P_train) / L
+    A_y = sum(P_train) - B_y
 
     % Create a vector with forces at locations
     w = zeros(n+1, 1);
     w(2) = A_y;
-    w(location) = -P;
+    w(locations) = -(P/6);
     w(L) = B_y;
 
     % Create SFD and BMD vectors with integration
     SFDi(i,:) = cumtrapz(w');
     BMDi(i,:) = cumtrapz(SFDi(i,:));
+    figure;
+    plot(SFDi(i,:), "LineWidth", 3);
+    figure;
+    plot(BMDi(i,:), "LineWidth", 3);
 end
 
 % Get max SFD, BMD and plot
@@ -39,7 +45,7 @@ end
 SFD = max(abs(SFDi));
 BMD = max(BMDi);
 
-figure;
-plot(SFD, "LineWidth", 3);
-figure;
-plot(BMD, "LineWidth", 3);
+% figure;
+% plot(SFD, "LineWidth", 3);
+% figure;
+% plot(BMD, "LineWidth", 3);
