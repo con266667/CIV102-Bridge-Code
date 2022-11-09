22143a2bced745a2eec698f0d0c4895a25092224
Connor Wilson <cnwilson29@gmail.com>
2022-11-09 16:05:40 -0500

    updates

--- Diagrams.m
+++ Diagrams.m
@@ -9,7 +9,7 @@ x = linspace(0, L, n+1); % x-axis (0 to 1201)
 
 x_train = [52 228 392 568 732 908]; % Train Load Locations
 P_train=[1 1 1 1 1 1]*P/6; 
-n_train = 1;
+n_train = 2400;
 
 SFDi = zeros(n_train, n+1); % 1 SFD for each train loc.
 BMDi = zeros(n_train, n+1); % 1 BMD for each train loc.
@@ -17,34 +17,32 @@ BMDi = zeros(n_train, n+1); % 1 BMD for each train loc.
 % Solve for SFD and BMD with the train at different locations
 
 for i = 1:n_train
-    %location = fix(L / (n_train+1) * i); % start location of train (300, 600, 900)
-    locations = x_train;
+    locations = x_train - 1200 + fix(i);
+    locations(0 > locations | locations > 1201) = 0;
+    loads = P_train;
+    loads(locations == 0) = 0;
+    
     
     % Reaction forces (Rearranged moment and Fy equations)
-    B_y = sum(locations .* P_train) / L;
-    A_y = sum(P_train) - B_y;
+    B_y = sum(locations .* loads) / L;
+    A_y = sum(loads) - B_y;
 
     % Create a vector with forces at locations
     w = zeros(n+1, 1);
     w(2) = A_y;
-    w(locations) = -(P/6);
+    w(locations(locations>0)) = w(locations(locations>0)) -(P/6);
     w(L) = B_y;
 
     % Create SFD and BMD vectors with integration
     SFDi(i,:) = cumtrapz(w');
     BMDi(i,:) = cumtrapz(SFDi(i,:));
-    figure;
-    plot(SFDi(i,:), "LineWidth", 3);
-    figure;
-    plot(BMDi(i,:), "LineWidth", 3);
 end
 
 % Get max SFD, BMD and plot
-
 SFD = max(abs(SFDi));
 BMD = max(BMDi);
 
-% figure;
-% plot(SFD, "LineWidth", 3);
-% figure;
-% plot(BMD, "LineWidth", 3);
+figure;
+plot(SFDi, "LineWidth", 3);
+figure;
+plot(BMDi, "LineWidth", 3);
